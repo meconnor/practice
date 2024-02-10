383b001ea7c20415f6e7f6d2750834d5f6c9f9ef
Connor <c.meriweather55@gmail.com>
2024-02-10 14:40:09 -0600

    grasping tuples

---
+++ practice.ts
@@ -0,0 +1,22 @@
+let myFunction = function (a: number, b?: number): number {
+    if (typeof b !== "undefined") {
+      return a + b + 5;
+    } else {
+      return a + 5;
+    }
+  };
+  
+  console.log(myFunction(1, 2));
+  console.log(myFunction(3, 5));
+  
+  
+  let guitarOne : [string, string, number];
+  guitarOne = ['electric', 'Ibanez', 7];
+  
+  let guitarTwo : [string, string, number];
+  guitarTwo = ['acoustic', 'Godin', 6];
+  
+  let guitarThree : [string, string, number];
+  guitarThree = ['electric', 'EB Music Man', 6];
+  
+  console.log(guitarThree);
